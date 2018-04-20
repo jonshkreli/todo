4e6e8176e6902feb7cb83105e8234664f5f9c0ba
Jon Shkreli <jon.shkreli.sp@lhind.dlh.de>
2018-04-21 00:07:56 +0200

    Added MaterialUI theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+        <MuiThemeProvider>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">To do</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Here will be the tasks...
         </p>
       </div>
+        </MuiThemeProvider>
     );
   }
 }
